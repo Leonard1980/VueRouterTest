027973a8ac469aa2a5994e98152382bf2509e3c2
Leonard1980 <Leonard1980@users.noreply.github.com>
2017-07-12 14:35:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import App from './App.vue';
-import router from './router';
-import Vue from 'vue';
+import App from './App.vue'
+import router from './router'
+import Vue from 'vue'
 new Vue({
     router,
     render: h => h(App)
